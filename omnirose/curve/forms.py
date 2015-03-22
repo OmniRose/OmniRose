49999de7ac753f57e3c25d9d36c1806f3ec3a0ee
Edmund von der Burg <evdb@ecclestoad.co.uk>
2015-03-22 08:40:03 +0100

    Customize degree widget so that if formats floats more elegantly

--- omnirose/curve/forms.py
+++ omnirose/curve/forms.py
@@ -4,8 +4,30 @@ from django.forms.widgets import NumberInput
 
 from .models import Reading
 
+class DegreeInput(NumberInput):
+
+    """Set the default style"""
+    def __init__(self, attrs=None):
+        if attrs is None:
+            attrs = {}
+        attrs['style'] = "width: 5em;"
+
+        super(DegreeInput, self).__init__(attrs)
+
+    """Strip decimal points if not needed"""
+    def _format_value(self, value):
+        return u"%g" % value
+
+
 class ReadingForm(forms.Form):
-    ships_head = forms.FloatField(required=False, widget=NumberInput(attrs={'style': "width: 5em;"}))
-    deviation  = forms.FloatField(required=False, widget=NumberInput(attrs={'style': "width: 5em;"}))
+    ships_head = forms.FloatField(
+        required=False,
+        widget=DegreeInput()
+    )
+
+    deviation = forms.FloatField(
+        required=False,
+        widget=DegreeInput()
+    )
 
 ReadingFormSet = formset_factory(form=ReadingForm)
