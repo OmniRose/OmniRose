78497ceba398fe98e0e98559076ef28c74371549
Edmund von der Burg <evdb@ecclestoad.co.uk>
2015-04-21 15:44:02 +0200

    Extract potentialy common code to base.py

---
+++ omnirose/outputs/base.py
@@ -0,0 +1,47 @@
+
+from .helpers import split_into_lines
+
+class OutputsTextMixin:
+    def draw_text_block(self, text, font_size, y, max_lines=2):
+        context = self.context
+        curve = self.curve
+
+        available_width = self.SURFACE_WIDTH - self.text_padding
+
+        with context:
+
+            while font_size > 0:
+
+                for line_count in range(1, max_lines+1):
+
+                    lines = split_into_lines(text, line_count)
+
+                    context.set_font_size(font_size)
+
+                    largest_width = 0
+
+                    for line in lines:
+                        (x_bearing, y_bearing, width, height, x_advance, y_advance) = context.text_extents(line)
+                        if width > largest_width:
+                            largest_width = width
+
+                    if largest_width < available_width:
+                        break
+
+                if largest_width < available_width:
+                    break
+                else:
+                    font_size = font_size - 1
+
+
+
+
+
+            for line in lines:
+                (x_bearing, y_bearing, width, height, x_advance, y_advance) = context.text_extents(line)
+                x = self.SURFACE_WIDTH/2 - width/2
+                context.move_to(x,y)
+                context.show_text(line)
+                y = y + self.text_line_height * height
+
+        return y
--- omnirose/outputs/table.py
+++ omnirose/outputs/table.py
@@ -7,14 +7,14 @@ from math import sin, cos, radians, pi
 import cairocffi as cairo
 
 from omnirose.templatetags.omnirose_tags import east_west
-from .helpers import split_into_lines
+from .base import OutputsTextMixin
 
 # http://en.wikipedia.org/wiki/Paper_size#PA_series
 # useful compromise between A4 and letter: 210mm × 280mm
 # In points this is: 595.275591 x 793.700787
 # Which approximates to 590 x 790 (shrunk a bit for safety)
 
-class Table:
+class Table(OutputsTextMixin):
     def __init__(self, curve=None, file_type='pdf', crop=False):
 
         self.is_cropped = crop
@@ -232,48 +232,5 @@ class Table:
         y = self.draw_text_block(curve.note, 18, y)
 
 
-    def draw_text_block(self, text, font_size, y, max_lines=2):
-        context = self.context
-        curve = self.curve
-
-        available_width = self.SURFACE_WIDTH - self.text_padding
-
-        with context:
-
-            while font_size > 0:
-
-                for line_count in range(1, max_lines+1):
-
-                    lines = split_into_lines(text, line_count)
-
-                    context.set_font_size(font_size)
-
-                    largest_width = 0
-
-                    for line in lines:
-                        (x_bearing, y_bearing, width, height, x_advance, y_advance) = context.text_extents(line)
-                        if width > largest_width:
-                            largest_width = width
-
-                    if largest_width < available_width:
-                        break
-
-                if largest_width < available_width:
-                    break
-                else:
-                    font_size = font_size - 1
-
-
-
-
-
-            for line in lines:
-                (x_bearing, y_bearing, width, height, x_advance, y_advance) = context.text_extents(line)
-                x = self.SURFACE_WIDTH/2 - width/2
-                context.move_to(x,y)
-                context.show_text(line)
-                y = y + self.text_line_height * height
-
-        return y
 
 
