d468b870c055761166cbfb7417fd79a8f8c752ea
Edmund von der Burg <evdb@ecclestoad.co.uk>
2015-04-21 09:46:29 +0200

    Reset all migrations

--- omnirose/curve/migrations/0001_initial.py
+++ omnirose/curve/migrations/0001_initial.py
@@ -18,8 +18,8 @@ class Migration(migrations.Migration):
             name='Curve',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('vessel', models.CharField(max_length=80)),
-                ('note', models.CharField(max_length=80)),
+                ('vessel', models.CharField(help_text=b'e.g. "SV Gypsy Moth"', max_length=80, verbose_name=b"Vessel's Name")),
+                ('note', models.CharField(help_text=b'e.g. "Steering compass" or "with radio mounted on binnacle"', max_length=80, verbose_name=b'Note', blank=True)),
                 ('equation_slug', models.CharField(blank=True, max_length=80, choices=[(b'trig_13', b'13 variable trig curve'), (b'trig_11', b'11 variable trig curve'), (b'trig_9', b'9 variable trig curve'), (b'trig_7', b'7 variable trig curve'), (b'trig_5', b'5 variable trig curve'), (b'trig_3', b'3 variable trig curve')])),
                 ('created', models.DateTimeField(auto_now_add=True)),
                 ('user', models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, null=True)),
