608ad031e3ab53ae0b41be80da7d2c098f4246a1
Edmund von der Burg <evdb@ecclestoad.co.uk>
2015-08-10 13:17:06 +0200

    Added about us page.

--- omnirose/omnirose/urls.py
+++ omnirose/omnirose/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.base import TemplateView
 from django.contrib import admin
 
 from .views import HomePageView
 
 urlpatterns = patterns('',
-    # Examples:
+
     url(r'^$', HomePageView.as_view(), name='home'),
+
+    url(r'about/', TemplateView.as_view(template_name='about.html'), name='about'),
 
     url(r'^accounts/', include('accounts.urls')),
     url(r'^curves/', include('curve.urls')),
