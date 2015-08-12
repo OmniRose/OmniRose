a53e3c91194edc93e92a0429aff517241a6de069
Edmund von der Burg <evdb@ecclestoad.co.uk>
2015-08-12 15:24:55 +0200

    Create png and cropped pngs of the rose and table

--- omnirose/omnirose/management/commands/omni_create_sample_svgs.py
+++ omnirose/omnirose/management/commands/omni_create_sample_svgs.py
@@ -16,16 +16,28 @@ class Command(BaseCommand):
         sample = samples['rya_training_almanac']
         curve = create_database_curve_from_sample(sample)
 
-        self.draw(Table(curve=curve), 'table')
-        self.draw(Rose(curve=curve, variation=-7), 'rose')
+        self.draw(Table(curve=curve), 'table', x=150, y=540)
+        self.draw(Rose(curve=curve, variation=-7), 'rose', x=269, y=520)
 
 
-    def draw(self, output, name):
+
+    def draw(self, output, name, x, y, height=200, width=200):
         output.draw()
         output.surface.finish()
 
-        destination = "omnirose/static/%s.svg" % name
-        os.system('pdf2svg %s %s' % (output.filename, destination))
+        destination_svg = "omnirose/static/%s.svg" % name
+
+        os.system('pdf2svg %s %s' % (output.filename, destination_svg))
         os.remove(output.filename)
+
+        destination_png = "omnirose/static/%s.png" % name
+        destination_cropped = "omnirose/static/%s_cropped.png" % name
+
+        inkscape_cmd = "inkscape %s --without-gui --export-dpi=180" % destination_svg
+        os.system(inkscape_cmd + " -e %s" % destination_png)
+        os.system(inkscape_cmd + " -e %s --export-area=%u:%u:%u:%u" % (destination_cropped, x, y, x + width, y + height))
+
+        for file in destination_png, destination_cropped:
+            os.system("optipng %s" % file)
 
         self.stdout.write('Successfully generated %s' % name)
