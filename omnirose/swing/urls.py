cc04157002e0eaa164a4b96ddead92351c932e71
Edmund von der Burg <evdb@ecclestoad.co.uk>
2015-10-27 17:27:02 +0100

    Delete demo code for swings done by driving boat round in circles

--- omnirose/swing/urls.py
+++ omnirose/swing/urls.py
@@ -9,8 +9,6 @@ urlpatterns = [
 
     url(r'^yours/', views.YourSunSwingListView.as_view(), name='swing_list'),
 
-    # url(r'demo/', TemplateView.as_view(template_name='swing/demo.html'), name='swing_demo'),
-
     url(r'sun/',  TemplateView.as_view(template_name='swing/sun.html'), name='sun_demo'),
     url(r'sun_json/', views.JsonPostView.as_view(), name="sun_json"),
 ]
