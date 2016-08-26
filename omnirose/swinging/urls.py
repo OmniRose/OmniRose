4a7c0e1629f54c6b94dbf027021e2ce3f567f104
Edmund von der Burg <evdb@ecclestoad.co.uk>
2016-08-26 14:50:45 +0200

    Sun swinging changes

--- omnirose/swinging/urls.py
+++ omnirose/swinging/urls.py
@@ -6,6 +6,8 @@ from . import views
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='swinging/home.html'), name='swinging_home'),
     url(r'^compass_theory/$', TemplateView.as_view(template_name='swinging/compass_theory.html'), name='swinging_compass_theory'),
+
+    url(r'^sun/$', TemplateView.as_view(template_name='swinging/sun_home.html'), name='swinging_sun_home'),
 ]
 
 
